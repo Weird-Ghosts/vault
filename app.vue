1483a0d19b70f34433b4696f09a4135362b7a389
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-07-25 15:24:29 -0400

    Init commit!

---
+++ app.vue
@@ -0,0 +1,41 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style lang="postcss">
+@font-face {
+  font-family: "PP Object Sans";
+  src: url("/PPObjectSans-Regular.eot");
+  src: url("/PPObjectSans-Regular.eot?#iefix") format("embedded-opentype"),
+    url("/PPObjectSans-Regular.woff2") format("woff2"),
+    url("/PPObjectSans-Regular.woff") format("woff"),
+    url("/PPObjectSans-Regular.ttf") format("truetype");
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "PP Object Sans";
+  src: url("/PPObjectSans-Black.eot");
+  src: url("/PPObjectSans-Black.eot?#iefix") format("embedded-opentype"),
+    url("/PPObjectSans-Black.woff2") format("woff2"),
+    url("/PPObjectSans-Black.woff") format("woff"),
+    url("/PPObjectSans-Black.ttf") format("truetype");
+  font-weight: 900;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "PP Object Sans";
+  src: url("/ObjectSans-Bold.eot");
+  src: url("/ObjectSans-Bold.eot?#iefix") format("embedded-opentype"),
+    url("/ObjectSans-Bold.woff2") format("woff2"),
+    url("/ObjectSans-Bold.woff") format("woff"),
+    url("/ObjectSans-Bold.ttf") format("truetype");
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+</style>
