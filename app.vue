da659a0c799f548ee3dc6444dec134c243886d1d
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-07-26 13:12:11 -0400

    Seeing if path fizses fonts

--- app.vue
+++ app.vue
@@ -10,11 +10,11 @@ body {
 }
 @font-face {
   font-family: "PP Object Sans";
-  src: url("@/PPObjectSans-Regular.eot");
-  src: url("@/PPObjectSans-Regular.eot?#iefix") format("embedded-opentype"),
-    url("@/PPObjectSans-Regular.woff2") format("woff2"),
-    url("@/PPObjectSans-Regular.woff") format("woff"),
-    url("@/PPObjectSans-Regular.ttf") format("truetype");
+  src: url("/PPObjectSans-Regular.eot");
+  src: url("/PPObjectSans-Regular.eot?#iefix") format("embedded-opentype"),
+    url("/PPObjectSans-Regular.woff2") format("woff2"),
+    url("/PPObjectSans-Regular.woff") format("woff"),
+    url("/PPObjectSans-Regular.ttf") format("truetype");
   font-weight: normal;
   font-style: normal;
   font-display: swap;
@@ -22,11 +22,11 @@ body {
 
 @font-face {
   font-family: "PP Object Sans";
-  src: url("@/PPObjectSans-Black.eot");
-  src: url("@/PPObjectSans-Black.eot?#iefix") format("embedded-opentype"),
-    url("@/PPObjectSans-Black.woff2") format("woff2"),
-    url("@/PPObjectSans-Black.woff") format("woff"),
-    url("@/PPObjectSans-Black.ttf") format("truetype");
+  src: url("/PPObjectSans-Black.eot");
+  src: url("/PPObjectSans-Black.eot?#iefix") format("embedded-opentype"),
+    url("/PPObjectSans-Black.woff2") format("woff2"),
+    url("/PPObjectSans-Black.woff") format("woff"),
+    url("/PPObjectSans-Black.ttf") format("truetype");
   font-weight: 900;
   font-style: normal;
   font-display: swap;
@@ -34,11 +34,11 @@ body {
 
 @font-face {
   font-family: "PP Object Sans";
-  src: url("@/ObjectSans-Bold.eot");
-  src: url("@/ObjectSans-Bold.eot?#iefix") format("embedded-opentype"),
-    url("@/ObjectSans-Bold.woff2") format("woff2"),
-    url("@/ObjectSans-Bold.woff") format("woff"),
-    url("@/ObjectSans-Bold.ttf") format("truetype");
+  src: url("/ObjectSans-Bold.eot");
+  src: url("/ObjectSans-Bold.eot?#iefix") format("embedded-opentype"),
+    url("/ObjectSans-Bold.woff2") format("woff2"),
+    url("/ObjectSans-Bold.woff") format("woff"),
+    url("/ObjectSans-Bold.ttf") format("truetype");
   font-weight: 700;
   font-style: normal;
   font-display: swap;
