3634c055263b942bd042f72f1e5f1b3ab0ad1ccd
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-08-08 13:13:29 -0400

    Add toc depth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
-
+  extends: "@nuxt-themes/docus",
+  content: {
+    markdown: {
+      remarkPlugins: ["remark-smartypants"],
+      toc: {
+        depth: 4,
+        searchDepth: 4,
+      },
+    },
+  },
   modules: [
     // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
+    "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
+    "@nuxt/devtools",
   ],
-  
-})
+});
